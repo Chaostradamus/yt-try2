8ca81661b358a31da07a55830191dd50d1399a23
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2021-09-21 02:04:03 -0400

    pre v2

--- components/VideoListItem/VideoListItem.tsx
+++ components/VideoListItem/VideoListItem.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { View, Text, Image, Pressable } from 'react-native';
-import { Entypo } from '@expo/vector-icons';
-import styles from './styles';
+import React from "react";
+import { View, Text, Image, StyleSheet, Pressable } from "react-native";
+import { Entypo } from "@expo/vector-icons";
+import styles from "./styles";
 
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation } from "@react-navigation/native";
 
 type VideoListItemProps = {
   video: {
@@ -17,56 +17,73 @@ type VideoListItemProps = {
     user: {
       name: string;
       image?: string;
-    }
-  }
-}
-
+    };
+  };
+};
 
 const VideoListItem = (props: VideoListItemProps) => {
   const { video } = props;
+  //   const minutes = Math.floor(video.duration / 60);
+  //   const seconds = video.duration % 60;
+
+  //   let viewsString = video.views.toString();
+  //   if (video.views > 1_000_000) {
+  //     viewsString = (video.views / 1_000_000).toFixed(1) + "m";
+  //   } else if (video.views > 1_000) {
+  //     viewsString = (video.views / 1_000).toFixed(1) + "k";
+  //   }
 
   const navigation = useNavigation();
 
-  const minutes = Math.floor(video.duration / 60);
-  const seconds = video.duration % 60;
-
-  let viewsString = video.views.toString();
-  if (video.views > 1_000_000){
-    viewsString = (video.views / 1_000_000).toFixed(1) + 'm'
-  } else if (video.views > 1_000) {
-    viewsString = (video.views / 1_000).toFixed(1) + 'k'
-  }
-
   const openVideoPage = () => {
     navigation.navigate("VideoScreen", { id: video.id });
-  }
-
+  };
   return (
     <Pressable onPress={openVideoPage} style={styles.videoCard}>
-      {/* Tumbnail */}
+      {/* {thumbnail} */}
+
       <View>
-        <Image style={styles.thumbnail} source={{ uri: video.thumbnail }} />
+        <Image
+          style={styles.thumbnail}
+          source={{
+            uri: video.thumbnail,
+          }}
+        />
         <View style={styles.timeContainer}>
-          <Text style={styles.time}>{minutes}:{seconds < 10 ? '0' : ''}{seconds}</Text>
+          <Text style={styles.time}>
+            {video.duration}
+            {/* {minutes}
+            :{seconds < 10 ? "0" : ""}
+            {seconds} */}
+          </Text>
         </View>
       </View>
 
-      {/* Title row */}
+      {/* {titlerow} */}
       <View style={styles.titleRow}>
-        {/* Avatar */}
-        <Image style={styles.avatar} source={{ uri: video.user.image }} />
+        {/* {avatar} */}
+        <Image
+          style={styles.avatar}
+          source={{
+            uri: video.user.image,
+          }}
+        />
 
-        {/* Middle container: Title, subtitle, etc. */}
+        {/* {middle container} */}
         <View style={styles.midleContainer}>
           <Text style={styles.title}>{video.title}</Text>
-          <Text style={styles.subtitle}>{video.user.name} {viewsString} {video.createdAt}</Text>
+          <Text style={styles.subtitle}>
+            {video.user.name}
+            {/* {viewsString} */}
+            {video.views} {video.createdAt}
+          </Text>
         </View>
-        
-        {/* Icon */}
-        <Entypo name="dots-three-vertical" size={16} color="white" />
+
+        {/* {icon} */}
+        <Entypo name="dots-three-vertical" size={18} color="white" />
       </View>
     </Pressable>
-  )
-}
+  );
+};
 
 export default VideoListItem;
